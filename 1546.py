4be75ab9e595c545fcbd965092127f6b43b5fbb2
djccnt15 <53306911+djccnt15@users.noreply.github.com>
2020-04-16 10:43:43 +0900

    Update 1546.py

--- 1546.py
+++ 1546.py
@@ -1,8 +1,4 @@
 # 1546
-a = int(input())
-score = list(map(int, input().split())) # 입력된 인풋을 int타입으로 리스트화
-new_score = []
-for i in score: new_score.append(i / max(score) * 100)
-r = 0
-for i in new_score: r += i
-print(r/len(new_score))
+b = int(input())
+a = list(map(int, input().split()))
+print((sum(a)/max(a)*100)/b)
